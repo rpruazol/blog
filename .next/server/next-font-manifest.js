6a3edd257a498ff452ab6d8486a6a57b526d7c55
ray <rpruazol@gmail.com>
2025-11-19 20:36:58 -0500

    Delete .next directory

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/rpruazol/blog/app/layout\":[\"static/media/f980ec13b5b5e554.p.woff2\",\"static/media/ec1a1eae803b668e-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
